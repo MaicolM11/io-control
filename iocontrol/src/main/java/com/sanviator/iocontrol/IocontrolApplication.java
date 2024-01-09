efaaa01a32c6f6686f547c2a2e1ace93e67f709c
Michael Alejandro Muñoz <mmoarias4@gmail.com>
2024-01-09 18:23:42 -0500

    Creacion proyecto

---
+++ iocontrol/src/main/java/com/sanviator/iocontrol/IocontrolApplication.java
@@ -0,0 +1,13 @@
+package com.sanviator.iocontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IocontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IocontrolApplication.class, args);
+	}
+
+}
